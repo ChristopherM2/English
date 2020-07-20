ca4fc9ef8f07eb0521773999ae6cff9f19fe8e8f
iwantcakenow21 <iwantcakenow21@gmail.com>
2020-07-19 20:37:39 -0400

    Add files via upload

--- Days left/src/TimeLeft.java
+++ Days left/src/TimeLeft.java
@@ -3,12 +3,21 @@ public class TimeLeft {
 	public static long timeLeft;
 
 	public static void main(String[] args) throws InterruptedException {
-		
 		timeLeft();
 	}
+
 	public static void timeLeft() {
-		while(true)		System.out.println("There is only " + ((1595908800000l - System.currentTimeMillis()) / 1000 / 60 / 60) + " Hours left until we are done english.");
-
-
+		while (true) {
+			if ((1595908800000l - System.currentTimeMillis() / 1000 / 60 / 60) > 0) {
+				System.out.println("There is only " + ((1595908800000l - System.currentTimeMillis()) / 1000 / 60 / 60)
+						+ " Hours left until we are done english.");
+			} else if ((1595908800000l - System.currentTimeMillis() / 1000 / 60 / 60) < 1) {
+				System.out.println("There is only " + ((1595908800000l - System.currentTimeMillis()) / 1000 / 60)
+						+ " Minitues left until we are done english.");
+			} else {
+				System.out.println("We have successfully escaped hell");
+			}
+			
+		}
 	}
 }
